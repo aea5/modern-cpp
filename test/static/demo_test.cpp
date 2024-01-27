88aac31c8639e0cca4d8a5d4a394ae752797cd0c
rivershade <cz5@duck.com>
2024-01-27 23:34:24 +0100

    update cmake

---
+++ test/static/demo_test.cpp
@@ -0,0 +1,10 @@
+#include <fenv.h>
+#include <iostream>
+#include <type_traits>
+
+namespace demotest
+{
+// some dummy static assert that is always true
+static_assert(std::is_same_v<int, int>, "int is not int");
+
+}
